ea1bbbe518138ffc5b76e5e2a4fd301b1d542608
Yashaswini <yashaswinisharma2001@gmail.com>
2021-06-16 04:56:01 +0530

    Update arraywithagivenSum.cpp

--- cpp/hashing/arraywithagivenSum.cpp
+++ cpp/hashing/arraywithagivenSum.cpp
@@ -7,7 +7,8 @@ bool subArray(vector<int> v, int sum){
     int presum=0;
     for(int i=0;i<v.size();i++){
         presum+=v[i];
-        if(s.find(sum-presum)!=s.end()&&!s.empty()) return true;
+        if(presum==sum) return true;
+        if(s.find(sum-presum)!=s.end()) return true;
         s.insert(presum);
     }
     return false;
