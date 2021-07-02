93c46e1dc8b09b8288a0b22721c954387b367140
Yashaswini <yashaswinisharma2001@gmail.com>
2021-07-02 23:50:01 +0530

    Create huffman.cpp

---
+++ cpp/greedy/huffman.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+#include<queue>
+using namespace std;
+class Node{
+    public:
+    char val;
+    int freq;
+    Node* left;
+    Node* right;
+
+    Node(char c, int v){
+        val=c;
+        freq=v;
+    }
+};
+int main(){
+
+}
