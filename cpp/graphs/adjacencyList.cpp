aa036f2a6c9261fdb7808dcd9f70554cda4a4959
Yashaswini <yashaswinisharma2001@gmail.com>
2021-07-03 05:47:40 +0530

    Create adjacencyList.cpp

---
+++ cpp/graphs/adjacencyList.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+void addEdge(vector<vector<int>> adj, int u, int v){
+    adj[u].push_back(v);
+    adj[v].push_back(u);
+}
+int main(){
+    int v=4;
+    vector<vector<int>> adj(v);
+    addEdge(adj,0,1);
+    addEdge(adj,0,2);
+    addEdge(adj,1,2);
+    addEdge(adj,1,2);
+    return 0;
+}
