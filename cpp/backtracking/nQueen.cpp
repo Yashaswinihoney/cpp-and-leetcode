00c8467e8a9e9c9109c99a713cffc33d7d7aded5
Yashaswini <yashaswinisharma2001@gmail.com>
2021-07-18 17:00:39 +0530

    nqueen

--- cpp/backtracking/nQueen.cpp
+++ cpp/backtracking/nQueen.cpp
@@ -15,8 +15,47 @@ void printsol(int board[N][N]){
     }
 }
 
-bool solverec(int col){
+bool isSafe(int row, int col){
 
+    //we just have to check before the regior
+    //where [row][col] 
+
+    //checking in the same row
+    for(int i=0;i<col;i++){
+        if(board[row][i]) 
+            return false;
+    }
+
+    //now checking in the upper diagonal before the point
+    for(int i=row, j=col;i>=0&&j>=0;i--,j--){
+        if(board[i][j]) 
+            return false;
+    }
+
+    //now checking in the lower diagonal after the point
+    for(int i=row, j=col;j>=0&&i<N;i++,j--){
+        if(board[i][j]) 
+            return false;
+    }
+
+    return true;
+}
+
+bool solverec(int col){
+    if(col==N) 
+        return true;
+
+    for(int i=0;i<N;i++){
+        if(isSafe(i,col)){
+            board[i][col]=1;
+
+            if(solverec(col+1)){
+                return true;
+            }
+
+            board[i][col]=0;
+        }
+    }
 }
 
 bool solve(){
