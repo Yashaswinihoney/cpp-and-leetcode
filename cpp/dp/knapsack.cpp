be68edec71107e20879ffdc7eb7703344585cc8c
Yashaswini <yashaswinisharma2001@gmail.com>
2021-08-14 17:07:28 +0530

    Update knapsack.cpp

--- cpp/dp/knapsack.cpp
+++ cpp/dp/knapsack.cpp
@@ -2,11 +2,25 @@
 #include <vector>
 #include <algorithm>
 #include <cstring>
+#include <cmath>
 using namespace std;
 
-int knapsack(int W, int wt[], int va[], int n){
+int knapsack(int W, int wt[], int val[], int n){
     int dp[n+1][W+1];
     memset(dp, 0, sizeof(dp));
+
+    for(int i=1;i<=n;i++){
+        for(int j=1;j<=W;j++){
+            if(wt[i-1]>j){
+                dp[i][j]=dp[i-1][j];
+            }
+            else{
+                dp[i][j]=max(val[i-1]+dp[i-1][j-wt[i-1]],dp[i-1][j]);
+            }
+        }
+    }
+
+    return dp[n][W];
 }
 int main(){
     int val[4]={10,40,30,50};
